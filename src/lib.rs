6cf51835453676d8e9f178fc74d1c8dc72c8ea88
shakencodes <shakencodes@gmail.com>
2023-10-30 22:36:40 -0700

    Update to support embedded-hal 1.0.0-rc.1

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,20 @@
 
 extern crate bit_field;
 extern crate cast;
-extern crate embedded_hal as hal;
+extern crate embedded_hal;
 
-use hal::blocking::i2c;
-use reg_conf::Configuration;
-use reg_device_id::DeviceId;
-use reg_manuf_id::ManufacturerId;
-use reg_res::Resolution;
-use reg_temp::Temperature;
-use reg_temp_alert_crit::CriticalTemperatureAlert;
-use reg_temp_alert_lower::LowerTemperatureAlert;
-use reg_temp_alert_upper::UpperTemperatureAlert;
+use crate::error::Error;
+use crate::reg_conf::Configuration;
+use crate::reg_device_id::DeviceId;
+use crate::reg_manuf_id::ManufacturerId;
+use crate::reg_res::Resolution;
+use crate::reg_temp::Temperature;
+use crate::reg_temp_alert_crit::CriticalTemperatureAlert;
+use crate::reg_temp_alert_lower::LowerTemperatureAlert;
+use crate::reg_temp_alert_upper::UpperTemperatureAlert;
+use embedded_hal::i2c::{I2c, SevenBitAddress};
 
+pub mod error;
 mod prelude;
 pub mod reg;
 pub mod reg_conf;
@@ -33,23 +35,16 @@ pub enum Address {
     Default = 0b0011000,
 }
 
-/// All possible errors in this crate
-#[derive(Debug)]
-pub enum Error<E> {
-    /// I2C bus error
-    I2c(E),
-    RegisterSizeMismatch(u8),
-}
-
 /// MCP9808 Driver
 pub struct MCP9808<I2C> {
     addr: u8,
     i2c: I2C,
 }
 
-impl<I2C, E> MCP9808<I2C>
+impl<I2C> MCP9808<I2C>
 where
-    I2C: i2c::Write<Error = E> + i2c::WriteRead<Error = E>,
+    I2C: I2c<SevenBitAddress>,
+    I2C::Error: Into<Error<I2C::Error>>,
 {
     /// Creates a new driver from an I2C peripheral.
     pub fn new(i2c: I2C) -> Self {
@@ -64,52 +59,53 @@ where
         self.i2c
     }
 
-    fn read_register<T>(&mut self, mut reg: T) -> Result<T, Error<E>>
+    fn read_register<T>(&mut self, mut reg: T) -> Result<T, Error<I2C::Error>>
     where
         T: prelude::Read,
-        I2C: i2c::WriteRead,
+        I2C: I2c<SevenBitAddress>,
+        I2C::Error: Into<Error<I2C::Error>>,
     {
-        reg.read_from_device(&mut self.i2c, self.addr)
-            .map_err(Error::I2c)?;
+        reg.read_from_device(&mut self.i2c, self.addr)?;
         Ok(reg)
     }
 
-    pub fn write_register<R: prelude::Write>(&mut self, reg: R) -> Result<(), Error<E>> {
-        reg.write_to_device(&mut self.i2c, self.addr)
-            .map_err(Error::I2c)?;
+    pub fn write_register<R: prelude::Write>(&mut self, reg: R) -> Result<(), Error<I2C::Error>> {
+        reg.write_to_device(&mut self.i2c, self.addr)?;
         Ok(())
     }
 
-    pub fn read_configuration(&mut self) -> Result<impl Configuration, Error<E>> {
+    pub fn read_configuration(&mut self) -> Result<impl Configuration, Error<I2C::Error>> {
         self.read_register(reg_conf::new())
     }
 
-    pub fn read_device_id(&mut self) -> Result<impl DeviceId, Error<E>> {
+    pub fn read_device_id(&mut self) -> Result<impl DeviceId, Error<I2C::Error>> {
         self.read_register(reg_device_id::new())
     }
 
-    pub fn read_manufacturer_id(&mut self) -> Result<impl ManufacturerId, Error<E>> {
+    pub fn read_manufacturer_id(&mut self) -> Result<impl ManufacturerId, Error<I2C::Error>> {
         self.read_register(reg_manuf_id::new())
     }
 
-    pub fn read_resolution(&mut self) -> Result<impl Resolution, Error<E>> {
+    pub fn read_resolution(&mut self) -> Result<impl Resolution, Error<I2C::Error>> {
         self.read_register(reg_res::new())
     }
 
     /// Read temperature register. Its double-buffered so no wait required.
-    pub fn read_temperature(&mut self) -> Result<impl Temperature, Error<E>> {
+    pub fn read_temperature(&mut self) -> Result<impl Temperature, Error<I2C::Error>> {
         self.read_register(reg_temp::new())
     }
 
-    pub fn read_alert_critical(&mut self) -> Result<impl CriticalTemperatureAlert, Error<E>> {
+    pub fn read_alert_critical(
+        &mut self,
+    ) -> Result<impl CriticalTemperatureAlert, Error<I2C::Error>> {
         self.read_register(reg_temp_alert_crit::new())
     }
 
-    pub fn read_alert_lower(&mut self) -> Result<impl LowerTemperatureAlert, Error<E>> {
+    pub fn read_alert_lower(&mut self) -> Result<impl LowerTemperatureAlert, Error<I2C::Error>> {
         self.read_register(reg_temp_alert_lower::new())
     }
 
-    pub fn read_alert_upper(&mut self) -> Result<impl UpperTemperatureAlert, Error<E>> {
+    pub fn read_alert_upper(&mut self) -> Result<impl UpperTemperatureAlert, Error<I2C::Error>> {
         self.read_register(reg_temp_alert_upper::new())
     }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,38 +1,52 @@
-use hal::blocking::i2c;
+use crate::error::Error;
+use crate::reg::Register;
 use core::fmt::Debug;
-use reg::Register;
+use embedded_hal::i2c::{I2c, SevenBitAddress};
 
 /// trait for a register that can be read from an i2c device
 pub trait Read: Debug + Copy + Clone {
-    fn read_from_device<I2C, E>(&mut self, i2c: &mut I2C, addr: u8) -> Result<(), E>
-        where I2C: i2c::WriteRead<Error=E>;
+    fn read_from_device<I2C>(&mut self, i2c: &mut I2C, addr: u8) -> Result<(), Error<I2C::Error>>
+    where
+        I2C: I2c<SevenBitAddress>,
+        I2C::Error: Into<Error<I2C::Error>>;
 }
 
 impl Read for Register {
-    fn read_from_device<I2C, E>(&mut self, i2c: &mut I2C, addr: u8) -> Result<(), E>
-        where I2C: i2c::WriteRead<Error=E> {
+    fn read_from_device<I2C>(&mut self, i2c: &mut I2C, addr: u8) -> Result<(), Error<I2C::Error>>
+    where
+        I2C: I2c<SevenBitAddress>,
+        I2C::Error: Into<Error<I2C::Error>>,
+    {
         let mut buf = [0u8; 2];
-        i2c.write_read(addr, &[self.get_ptr()], &mut buf[0..self.get_len() as usize])?;
+        i2c.write_read(
+            addr,
+            &[self.get_ptr()],
+            &mut buf[0..self.get_len() as usize],
+        )?;
         self.set_buf(buf);
         Ok(())
     }
 }
 
-
 /// trait for a register that can be written to an i2c device
 pub trait Write: Read {
-    fn write_to_device<I2C, E>(&self, i2c: &mut I2C, addr: u8) -> Result<(), E>
-        where I2C: i2c::Write<Error=E>;
+    fn write_to_device<I2C>(&self, i2c: &mut I2C, addr: u8) -> Result<(), Error<I2C::Error>>
+    where
+        I2C: I2c<SevenBitAddress>,
+        I2C::Error: Into<Error<I2C::Error>>;
 }
 
 impl Write for Register {
-    fn write_to_device<I2C, E>(&self, i2c: &mut I2C, addr: u8) -> Result<(), E>
-        where I2C: i2c::Write<Error=E> {
+    fn write_to_device<I2C>(&self, i2c: &mut I2C, addr: u8) -> Result<(), Error<I2C::Error>>
+    where
+        I2C: I2c<SevenBitAddress>,
+        I2C::Error: Into<Error<I2C::Error>>,
+    {
         // reg ptr + 1 or 2 bytes
         let mut buf = [self.get_ptr(); 3];
         for (i, item) in self.get_buf().iter().enumerate() {
             buf[i + 1] = *item;
         }
-        i2c.write(addr, &buf[0..self.get_len() as usize])
+        Ok(i2c.write(addr, &buf[0..self.get_len() as usize])?)
     }
 }
